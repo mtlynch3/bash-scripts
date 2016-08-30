22d49a491e50a05e1f26df4dadd01cf1d6f47293
Lane Schwartz <dowobeha@gmail.com>
2016-08-30 10:53:10 -0500

    Initial commit

---
+++ print_full_path.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###############################################################################
+# Instructions:
+###############
+#
+# Modify this script so that it prints to standard output 
+#  the full path from the root directory to the current directory
+#
+###############################################################################
+
+hhousaoheurchal urca,.gu.ucu
+# Students - delete this line and put your work here
---
+++ sorted_user_list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###############################################################################
+# Instructions:
+###############
+#
+# Modify this script so that it transforms the contents of the file /etc/password
+#  into a list of usernames (one username per line), sorted alphabetically,
+#  printing the output to standard output.
+#
+###############################################################################
+
+
+
+# Students - delete this line and put your work here
